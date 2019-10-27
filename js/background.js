c41ca9ea3a7d2bc8c1ddaa72b7d8d8890c732143
zraymene <chakib.tarzon@gmail.com>
2019-10-27 19:01:51 +0100

    Important changes
    • Found better way to communicate between POPUP-BACKGROUND
    • Updated NOTES

--- js/background.js
+++ js/background.js
@@ -1,7 +1,29 @@
-var current;
-chrome.runtime.onStartup.addListener(function() {
-  chrome.extension.onConnect.addListener(function(port) {
-      port.postMessage("START");
-      console.log('test');
-    });
-});
+chrome.extension.onConnect.addListener(function(port) {
+
+      console.log("Connected .....");
+
+      /* -------------------- SENDING / RECIVING MESSAGE SAMPLE ---------------
+      port.onMessage.addListener(function(msg) {
+           console.log("message recieved" + msg);
+           port.postMessage("Hi Popup.js");
+      });
+
+      */
+
+      port.onMessage.addListener(function(msg) {
+        /*   console.log(msg);
+           chrome.tabs.reload(msg.TAB.id);      // RELOAD TAB
+           setInterval(function(){ chrome.tabs.reload(msg.TAB.id); }, msg.INTERVAL * 1000); 
+          */
+           switch (msg.ACTION) {
+             case "START":
+                  console.log("Starting auto-refresher for TAB:" + msg.TAB.title);
+               break;
+             case "STOP":
+                  console.log("Stoping auto-refresher for TAB:" + msg.TAB.title);
+               break;
+           }
+
+      });
+
+ })
--- js/popup.js
+++ js/popup.js
@@ -1,34 +1,44 @@
+var port = chrome.extension.connect({
+      name: "SC"
+ });
+
+
+ /*      -----------      RECIVING MESSAGE SAMPLE     ----------------
+
+ port.onMessage.addListener(function(msg) {
+      console.log("message recieved" + msg);
+ });
+*/
+
 $(document).ready(function(){
 
-  var current ;
-
-  chrome.tabs.onActivated.addListener(function(tab) {
-    current = tab;
-  });
-
-  var port = chrome.runtime.connect();
-
-   port.onMessage.addListener(function(msg) {
-        switch (msg) {
-          case "START":
-            chrome.browserAction.setBadgeText({text: 'START'});
-            chrome.browserAction.setBadgeBackgroundColor({color: '#444444'});
-            console.log("Started");
-            break;
-
-        }
-   });
-
   $("#start").click(function() {
+
     chrome.browserAction.setBadgeText({text: 'ON'});
+
     chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
 
-    port.postMessage(tab);
+    chrome.tabs.getSelected(null, function(tab) {
+
+      $("#test").html(tab.id);
+
+      port.postMessage({
+        TAB : tab,
+        ACTION : "START",
+        INTERVAL : 1,
+        ERROR_TYPE : 0
+      });
+
+    });
+
   });
 
   $("#stop").click(function() {
+
     chrome.browserAction.setBadgeText({text: 'IDLE'});
+
     chrome.browserAction.setBadgeBackgroundColor({color: '#444444'});
+
   });
 
 
